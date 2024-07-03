65ac0e693d1b438148f3f9476c373efb83a90a0d
Genimax <urdnot.wrexx21@gmail.com>
2024-07-04 00:15:45 +0700

    convertation fixed

--- components/shared/ContributionPanel.js
+++ components/shared/ContributionPanel.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import useStore from "@/store";
 import ModalWindowShare from "@/components/shared/ModalWindowShare";
-import { extractCurrentConfig } from "@/utils/configTools";
+import config from "@/config/teamConfig.json";
 
 const ContributionPanel = ({ team }) => {
   let color;
@@ -14,19 +14,19 @@ const ContributionPanel = ({ team }) => {
   }
 
   const getUSDToTON = async () => {
-    // const response = await axios.get(
-    //   "https://min-api.cryptocompare.com/data/price?fsym=TON&tsyms=BTC,USD,EUR"
-    // );
-    // const data = response.data;
-    // console.log(data);
-    // return data["USD"];
+    const response = await axios.get(
+      "https://min-api.cryptocompare.com/data/price?fsym=TONCOIN&tsyms=USD"
+    );
+    const data = response.data;
+    console.log(data);
+    return data["USD"];
 
-    return 1.155;
+    return 7.867;
   };
 
   const [TONValue, setTONValue] = useState("");
   const [usdValue, setUsdValue] = useState("");
-  const [usdToTON, setUsdToTON] = useState(null);
+  const [usdToTON, setUsdToTON] = useState(1.128);
 
   const setModal = useStore((state) => state.setModal);
 
@@ -35,7 +35,7 @@ const ContributionPanel = ({ team }) => {
   };
 
   useEffect(() => {
-    if (!usdToTON) getUSDToTON().then((usd) => setUsdToTON(usd));
+    getUSDToTON().then((usd) => setUsdToTON(usd));
   }, []);
 
   const [lastUpdated, setLastUpdated] = useState(null);
@@ -43,7 +43,7 @@ const ContributionPanel = ({ team }) => {
   // Эффект для отслеживания изменений TONValue
   useEffect(() => {
     if (TONValue && usdToTON && lastUpdated === "TON") {
-      const newUsdValue = Math.round(TONValue * usdToTON);
+      const newUsdValue = Math.round(usdToTON * TONValue);
       setUsdValue(newUsdValue);
       setLastUpdated(null); // сбросить последнее обновление
     }
@@ -52,7 +52,7 @@ const ContributionPanel = ({ team }) => {
   // Эффект для отслеживания изменений usdValue
   useEffect(() => {
     if (usdValue && usdToTON && lastUpdated === "usd") {
-      const newTONValue = (usdValue / usdToTON).toFixed(5);
+      const newTONValue = (usdToTON / usdValue).toFixed(5);
       setTONValue(newTONValue);
       setLastUpdated(null); // сбросить последнее обновление
     }
@@ -70,8 +70,8 @@ const ContributionPanel = ({ team }) => {
     setLastUpdated("usd");
   };
 
-  const usdMin = extractCurrentConfig().minimumUSD;
-  const TONMin = usdToTON ? (usdMin / usdToTON).toFixed(4) : 0.85616 * usdMin;
+  const usdMin = config.minimumUSD;
+  const TONMin = (usdMin / usdToTON).toFixed(4);
 
   return (
     <div className="flex flex-col gap-4 w-1/3 text-xl min-w-60">
